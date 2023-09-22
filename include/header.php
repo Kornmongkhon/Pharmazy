df78b380bac9a900aa8f6858554efda44bede27a
skyconnor30 <s6404062610391@email.kmutnb.ac.th>
2023-09-23 05:40:11 +0700

    change navbar use bootstrap

--- include/header.php
+++ include/header.php
@@ -14,43 +14,65 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style/navbar/navbar.css">
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </head>
 <body>
-    <?php 
-        $User_login = isset($_GET['u_username']) ? $_GET['u_username'] : "";
-    ?>
-    <header>
-        <img class="logo" src="assets/images/mentoslogo.png" style="width: 400px;height:120px;">
-        <!-- <h1 style="color:white">Mentos Pharmazy</h1> -->
-        <div>
-            
-        </div>
-        <nav>
-            <ul class="nav-links">
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Promotion</a></li>
-                <li><a href="#">Contact</a></li>
-                <li><a href="#">Info</a></li>
+    <nav class="navbar navbar-expand-lg">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php"><img class="logo" src="assets/images/mentoslogo.png" style="width: 400px;height:120px;"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle active" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Category
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <li><a class="dropdown-item" href="#">Action</a></li>
+                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                </ul>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Promotions</a>
+                </li>
             </ul>
-        </nav>
-        <!-- check user login -->
-        <?php if(!isset($_SESSION['user_login'])):?>
-                <div class='button'>
-                    <a href='login.php' >Login</a> / <a href='register.php'>Sign Up</a>
-                </div>
-        <?php endif?>
-        <?php if(isset($_SESSION['user_login'])):?>
-            <?php
-                $showName = $pdo->prepare("SELECT * FROM users");
-                $showName->execute();
-                $row = $showName->fetch(PDO::FETCH_ASSOC);
-            ?>
-            
-            <div class='button'>
-                <span>welcome, <?=$row['u_username']?></span>
-                <a href="logout.php">Logut</a>
+            <form class="d-flex p-3 w-50">
+                <input class="form-control me-2 wd" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+            <?php if(!isset($_SESSION['user_login'])):?>
+                    <a class="btn btn-outline-success" style="margin-right: 15px;" href="login.php">Login</a>
+                    <a class="btn btn-outline-success" style="margin-right: 15px;" href="Register.php">Register</a>
+            <?php endif;?>
+            <?php if(isset($_SESSION['user_login'])):?>
+                <?php
+                    $showName = $pdo->prepare("SELECT * FROM users");
+                    $showName->execute();
+                    $row = $showName->fetch(PDO::FETCH_ASSOC);
+                ?>
+                <ul class="navbar-nav">
+                    <li class="nav-item dropdown" style="margin-right: 15px;">
+                        <a class="nav-link dropdown-toggle active" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Welcome, <?=$row['u_username']?>
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#">Profile</a></li>
+                            <li><a class="dropdown-item" href="#">Carts</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="logout.php">Logout</a></li>
+                        </ul>
+                    </li>
+                </ul>     
+            <?php endif;?>
             </div>
-        <?php endif;?>
-    </header>
-
+        </div>
+    </nav>
 </body>
