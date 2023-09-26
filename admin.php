48a8cac441b9630bd368b3c5a4bb036f9559dae3
skyconnor30 <s6404062610391@email.kmutnb.ac.th>
2023-09-27 04:14:25 +0700

    add admin page tio show user info example(10%)

---
+++ admin.php
@@ -0,0 +1,71 @@
+<?php
+session_start();
+include('include\head.php');
+include('include\functions.php');
+if (!isset($_SESSION['admin_login'])) {
+    echo "false";
+    header("location: login.php");
+}
+?>
+
+<head>
+    <link rel="stylesheet" href="style/admin/admin.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <?php if (isset($_SESSION['admin_login'])) : ?>
+        <?php
+        $stmt = $pdo->prepare("SELECT * FROM users WHERE urole = 'user'");
+        $stmt->execute();
+        ?>
+        <div class="container">
+            <form class="card login-card-custom  table-responsive-md" method="post">
+                <div class="title">Admin Mentos Page</div>
+                <section>User information</section>
+                <article class="user-details">
+                    <table class="table">
+                        <thead class="table-info">
+                            <tr style="align-items: center;text-align: center;" >
+                                <th >User ID</th>
+                                <th >Profile</th>
+                                <th >Username</th>
+                                <th class="col-1">Full Name</th>
+                                <th >Email</th>
+                                <th >Address</th>
+                                <th >Phone</th>
+                                <th >Gender</th>
+                                <th >Role</th>
+                                <th >Create At</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php while ($row = $stmt->fetch()) : ?>
+                                <?php 
+                                    $relativeAvatarPath = str_replace('../', '', $row['avatar']);
+                                ?>
+                                <tr style="align-items: center;text-align: center;">
+                                    <td><?= $row['uid'] ?></td>
+                                    <td><img src="<?=$relativeAvatarPath ?>" width="50" height="50" class="rounded-circle"></td>
+                                    <td><?=$row['u_username']?></td>
+                                    <td><?=$row['u_name']?></td>
+                                    <td><?=$row['email']?></td>
+                                    <td><?=$row['address']?></td>
+                                    <td ><?=$row['phone']?></td>
+                                    <td><?=$row['gender']?></td>
+                                    <td><?=$row['urole']?></td>
+                                    <td ><?=$row['create_at']?></td>
+                                </tr>
+                            <?php endwhile; ?>
+                        </tbody>
+                    </table>
+                </article>
+                <button class="btnbg-custom btn-lg mb-3" style="width: 50%;margin-left: auto;margin-right: auto;"><a href="logout_admin.php">Log out</a></button>
+            </form>
+        </div>
+    <?php endif; ?>
+</body>
+
+</html>
