d4b2bb2cf7bd97cc59264d555d6f43c5f5a14036
skyconnor30 <s6404062610391@email.kmutnb.ac.th>
2023-10-18 02:26:00 +0700

    add navbar responsive

--- include/header.php
+++ include/header.php
@@ -17,8 +17,8 @@
     <nav class="navbar navbar-expand-lg">
         <div class="container-fluid">
             <a class="navbar-brand" href="index.php"><img class="logo" src="assets/images/mentos.png" style="width: 250px;height:80px;"></a>
-            <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> -->
-            <span class="navbar-toggler-icon"></span>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <i class="fa-solid fa-bars text-white fa-lg"></i>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
