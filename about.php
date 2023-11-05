cbde7a578e99ba90dd803a5ce089f851a1e58b9a
skyconnor30 <s6404062610391@email.kmutnb.ac.th>
2023-11-05 21:37:29 +0700

    add JSON file and show to DataTable

--- about.php
+++ about.php
@@ -1,20 +1,23 @@
-<?php 
-    include('include\head.php');
-    if(isset($_COOKIE['visit'])){
-        $visit = $_COOKIE["visit"]+1;
-        setcookie('visit',$visit);
-    }
+<?php
+include('include\head.php');
+if (isset($_COOKIE['visit'])) {
+    $visit = $_COOKIE["visit"] + 1;
+    setcookie('visit', $visit);
+}
 ?>
+
 <head>
     <link rel="stylesheet" href="style/about/about.css">
+    <script src="style/about/about.js"></script>
 </head>
+
 <body>
     <div>
         <header>
-            <?php 
-                include('include\header.php');
+            <?php
+            include('include\header.php');
             ?>
-        </header> 
+        </header>
         <div class="container">
 
         </div>
@@ -24,40 +27,40 @@
                     <div class="hero-info">
                         <h3>MENTOS PHARMAZY คือใคร</h3>
                         <p>ร้านขายยา ที่มียาครบวงจร เรื่องสุขภาพ เรื่องผิวหน้า ที่นี่ที่เดียวครบวงจร</p>
-                        
-                     </div>
 
-                    
+                    </div>
+
+
                 </div>
             </div>
-        
+
         </section>
     </div>
 
     <div class="container">
 
-</div>
-<section class="About">
-    <div class="container">
-        <div class="About-con">
-            <div class="About-info">
-                <h3>เกี่ยวกับเรา</h3>
-                <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Pharmacy is the science and practice of discovering, producing, preparing, dispensing, reviewing and monitoring medications, aiming to ensure the safe, effective, and affordable use of medicines. It is a miscellaneous science as it links health sciences with pharmaceutical sciences and natural sciences. The professional practice is becoming more clinically oriented as most of the drugs are now manufactured by pharmaceutical industries. Based on the setting, pharmacy practice is either classified as community or institutional pharmacy. Providing direct patient care in the community of institutional pharmacies is considered clinical pharmacy.</p>
-             </div>
-
-             <div class="hero-img"> 
-                <img src="assets/images/pharmazy.jpg">
-             </div>
-        </div>
     </div>
+    <section class="About">
+        <div class="container">
+            <div class="About-con">
+                <div class="About-info">
+                    <h3>เกี่ยวกับเรา</h3>
+                    <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Pharmacy is the science and practice of discovering, producing, preparing, dispensing, reviewing and monitoring medications, aiming to ensure the safe, effective, and affordable use of medicines. It is a miscellaneous science as it links health sciences with pharmaceutical sciences and natural sciences. The professional practice is becoming more clinically oriented as most of the drugs are now manufactured by pharmaceutical industries. Based on the setting, pharmacy practice is either classified as community or institutional pharmacy. Providing direct patient care in the community of institutional pharmacies is considered clinical pharmacy.</p>
+                </div>
 
-</section>
-</div>
+                <div class="hero-img">
+                    <img src="assets/images/pharmazy.jpg">
+                </div>
+            </div>
+        </div>
+
+    </section>
+    </div>
 
     <section class="blog">
         <div class="container">
 
-            <div class="blog-title"> 
+            <div class="blog-title">
                 <h3>ผู้จ่ายยาของเรา</h3>
             </div>
 
@@ -68,7 +71,7 @@
                     <h4>เภสัชกร</h4>
                     <h5>ปริญญาเอก ภาควิชาวิทยาการคอมพิวเตอร์</h5>
                     <p>มีประสบการณ์เภสัชกรร้านขายยาเดียวและร้านขายยา Mexcio มายาวนานกว่า 10 ปี</p>
-                    
+
                 </div>
 
                 <div class="blog-item">
@@ -76,7 +79,7 @@
                     <h4>ผู้ช่วยเภสัชกร</h4>
                     <h5>ปริญญาเอก ภาควิชาไฟฟ้ากำลัง</h5>
                     <p>มีความเชียวชาญด้านยา อาหารเสริม สกินแคร์ มีประสบการณ์ร้านขายยาที่ Argentina 5 ปี</p>
-                    
+
                 </div>
 
                 <div class="blog-item">
@@ -84,15 +87,30 @@
                     <h4>จัดจำหน่าย</h4>
                     <h5>ปริญญาเอก ภาควิชาสถาปัตยกรรม</h5>
                     <p>มีประสบการณ์ร้านขายยามากกว่า 12 ปี เคยอยู่ร้านยา France มาหลายที่</p>
-                    
+
                 </div>
 
             </div>
 
         </div>
-
     </section>
 
+    <article class="container">
+            <h4 class="text-center text-dark alert alert-info"><i class="fa-solid fa-file"></i> รายการสินค้าอุปกรณ์การเเพทย์</h4>
+            <table id="MedicalTable" class="table table-responsive-md">
+                <thead class="table-info">
+                    <th>ลำดับที่</th>
+                    <th>ชื่ออังกฤษ</th>
+                    <th>ชื่อไทย</th>
+                    <th>ระดับความเสี่ยง</th>
+                    <th>ระบบไฟฟ้า</th>
+                    <th>การฆ่าเชื้อ</th>
+                    </tr>
+                </thead>
+                <tbody id="result">
+                </tbody>
+            </table>
+    </article>
     <div class="footer-top">
         <div class="container">
             <div class="footer-top-con">
@@ -135,13 +153,13 @@
                     <p><img src="assets/images/ig.png"> <span class="space">imtrex_.png</span></p>
                 </div>
                 <div class="footer-top-item" style="display: flex;">
-                    <h5>จำนวนคนเข้าชมเว็ปไซต์ในวันนี้</h5> 
+                    <h5>จำนวนคนเข้าชมเว็ปไซต์ในวันนี้</h5>
                     <span style="margin:0rem 1rem;" id="visit-count">
-                        <?php if(isset($_COOKIE['visit'])):?>
-                            <?=$visit?>
-                        <?php else:?>
-                            <?=0?>
-                        <?php endif;?>
+                        <?php if (isset($_COOKIE['visit'])) : ?>
+                            <?= $visit ?>
+                        <?php else : ?>
+                            <?= 0 ?>
+                        <?php endif; ?>
                     </span>
                     <h5>คน</h5>
                 </div>
@@ -152,6 +170,10 @@
     <div class="footer-bottom">
         <p>Copyright 2023 ©. Mentos Pharmazy (When ever you're sick, think of us)</p>
     </div>
-            
+    <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+    <script>
+        let table = new DataTable('#MedicalTable');
+    </script>
 </body>
+
 </html>
